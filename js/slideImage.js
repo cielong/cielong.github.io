7bd003a5e9e274659474eaaa9080753156cc18bf
cielong <yutian.wu@mail.com>
2018-01-21 15:55:08 -0500

    fix javascript fail problem

--- js/slideImage.js
+++ js/slideImage.js
@@ -1,8 +1,3 @@
-/*
-  This script is copied and modified from W3 school 
-  [https://www.w3schools.com/howto/howto_css_modal_images.asp] with modification;
-*/
-
 var slideIndex = 1;
 slideDivs();
 
@@ -12,30 +7,17 @@ function slideDivs() {
   setTimeout(slideDivs, 10000);
 }
 
-function currentDiv(n) {
-    showDivs(slideIndex=n);
-}
-
 function showDiv(n) {
     var i;
-    var x = document.getElementsByClassName("cover-img");
-    var dots = document.getElementsByClassName("badge");
+    var x = document.getElementsByClassName("cover-image");
     if (n > x.length) {
         slideIndex = 1;
     }
     if (n < 1) {
         slideIndex = x.length;
     }
-
     for (i=0; i < x.length; i++) {
         x[i].style.display = "none";
     }
-
-    for (i=0; i<dots.length;i++) {
-        dots[i].className = dots[i].className.replace(" badge-white", "");
-    }
-
     x[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " badge-white";
 }
-
